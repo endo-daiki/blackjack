97d78f02c173c18cced95a02877dd070fc61f3d1
daiki endo <daiki endo@EXCITE-1901-02>
2023-04-20 11:54:14 +0900

    新規登録機能の作成中

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,11 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User {
 	private int id;
 	private String name;
@@ -9,6 +15,39 @@ public class User {
 	private int win;
 	private int lose;
 	private int draw;
+	
+	public User() {}
+	public void insertUser(String name, String nicname, String password) {
+		Connection con = null;
+		try {
+			 con = DriverManager.getConnection(
+				      "jdbc:mysql://localhost/＜データベース名＞?useSSL=false",
+				      "root",
+				      ""
+				    );
+			 
+			 PreparedStatement pstmt = con.prepareStatement
+					 ("insert into User (name, nickname, password) values (?,?,?)");
+			 
+			 pstmt.setString(1, name);
+			 pstmt.setString(2, nickname);
+			 pstmt.setString(3, password);
+			 ResultSet rs = pstmt.executeQuery();
+			 
+			 rs.close();
+			 pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
 	
 }
