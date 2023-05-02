da578696180cecf2c5e8eaf9820c9b554218b73f
daiki endo <daiki endo@EXCITE-1901-02>
2023-05-02 17:30:42 +0900

    5月2日の作業分

--- src/main/java/servlet/HitServlet.java
+++ src/main/java/servlet/HitServlet.java
@@ -33,7 +33,6 @@ public class HitServlet extends HttpServlet {
 			HttpServletResponse response) 
 					throws ServletException, IOException {
 		
-		
 		String url = Blackjack.Hit(request);
 		
 		response.sendRedirect(url);
