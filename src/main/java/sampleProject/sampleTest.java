e63c60da18eb166104f83d24b54726d794db1d0a
daiki endo <daiki endo@EXCITE-1901-02>
2023-05-22 15:40:08 +0900

    テストコード

--- src/main/java/sampleProject/sampleTest.java
+++ src/main/java/sampleProject/sampleTest.java
@@ -2,9 +2,18 @@ package sampleProject;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import login.Login;
 import model.Card;
+import model.User;
 
 class sampleTest {
 
@@ -13,6 +22,21 @@ class sampleTest {
 		Card card = new Card("heart", "1");
 		assertEquals("heart", card.suit);
 		assertEquals("1", card.no);
+	}
+	
+	@Test
+	public void instantiate2() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		
+		User user = new User();
+		RequestDispatcher dispatcher = Login.login(user, request);
+		try {
+			dispatcher.forward(request, response);
+		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+		}		
+		assertEquals("IDを入力してください", (String)request.getAttribute("error_msg"));
 	}
 
 }
