8c8b862970e2d37729985c7555ee7e7ecbfa3ad0
daiki endo <daiki endo@EXCITE-1901-02>
2023-05-25 15:50:33 +0900

    Handクラスのテスト作成

---
+++ src/main/java/gameSystem/HandTest.java
@@ -0,0 +1,56 @@
+package gameSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Card;
+
+class HandTest {
+	static Hand hand;
+	static Deck deck;
+	
+	@BeforeEach
+	public void setup() {
+		hand = new Hand();
+		deck = new Deck();
+	}
+	
+	@Test
+	public void testHand() {
+		assertNotNull(hand);
+		assertEquals(false, hand.aceCheck());
+	}
+	
+	@Test
+	public void testDraw() {
+		hand.draw(deck);
+		List<Card> list = hand.getList();
+			
+		assertEquals(1, list.size());
+	}
+	
+	@Test
+	public void testAceCheck() {
+		assertEquals(false, hand.aceCheck());
+	}
+	
+	@Test
+	public void testGetList() {
+		List<Card> list = hand.getList();	
+		assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void testSplitCheck() {
+		hand.draw(deck);
+		hand.draw(deck);
+		hand.draw(deck);
+		assertEquals(false, hand.splitCheck());
+	}
+
+}
